ca5e8fa6de36e5863804428e3323742e7a3a6107
chun3720 <chun3720@hotmail.com>
2022-05-11 13:07:46 +0900

    Update Player.py

--- Player.py
+++ Player.py
@@ -12,10 +12,12 @@ import ForMatplotlib
 
 path = os.getcwd()
 code_list = [_ for _ in os.listdir(path) if _.endswith(".py")]
+to_ignore = ["__init__.py", "loadexp.py", "Player.py"]
+codes = [_ for _ in code_list if _ not in to_ignore]
 
 code_dict = {}
 
-for i, code in enumerate(code_list):
+for i, code in enumerate(codes):
     code_dict[i] = code
     
 
@@ -23,27 +25,16 @@ for key, value in code_dict.items():
     print(f'{key} : {value}')
 
 
-
-
-
-
-
-
-
-
 selector = input("which file want to run: ")
 chosen = int(selector)
 
-
 to_import = code_dict[chosen][:-3]
 
-# from ForMatplotlib import to_import
-# main()
 
 package_to_load = f'ForMatplotlib.{to_import}'
 module = importlib.import_module(package_to_load)
     
-# importlib.resources.files(to_import)
+
 print("\n\n")
 print(f"Current package: {package_to_load}\n")
 module.main()
